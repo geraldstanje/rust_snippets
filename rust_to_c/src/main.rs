721cb33e63de58cb0495f90ab645560f4ec67089
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-05-02 18:00:58 +0200

    added test

--- rust_to_c/src/main.rs
+++ rust_to_c/src/main.rs
@@ -4,6 +4,11 @@ extern {
     fn add(input1: libc::c_int, input2: libc::c_int) -> libc::c_int;
 }
 
+#[test]
+fn it_works() {
+    assert_eq!(3, unsafe { add(1, 2) });
+}
+
 fn main() {
     let input1 = 4;
     let input2 = 3;
