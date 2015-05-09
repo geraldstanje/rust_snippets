0d69c1d13c7e0a328e97f43a25bcd0983f746434
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-05-09 22:32:40 +0200

    added comments

--- iterator_fun/src/main.rs
+++ iterator_fun/src/main.rs
@@ -13,10 +13,14 @@ fn main() {
         println!("{}", num);
     }
     
+    // x.iter() ... iter() borrows x and returns an iterator to it
+    // and once the iterator is dropped you can use x again
     for num in nums.iter() {
         println!("{}", num);
     }
     
+    // x.into_iter() ... it borrows x and returns borrowed references into x
+    // with into_iter you can never use x again because it moved away
     for num in nums.into_iter() {
         println!("{}", num);
     }
