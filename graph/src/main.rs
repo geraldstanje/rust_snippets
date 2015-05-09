dd78ec328b6cebb00f22cb0a1cd68e27960eca3a
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-05-09 14:56:46 +0200

    fixed bug in dfs

--- graph/src/main.rs
+++ graph/src/main.rs
@@ -23,11 +23,6 @@ impl Graph {
     pub fn new() -> Graph { Graph{nodes: Vec::new(),} }
 
     pub fn add_edge(&mut self, src: u32, dst: u32, weight: f32) {
-        // only works with rust nightly:
-        // if (max(src,dst)) > self.nodes.len() as u32 {
-        //  self.nodes.resize((max(src, dst) + 1) as usize, Vec::new());
-        //}
-
         let len = self.nodes.len();
         if (max(src, dst)) > len as u32 {
             let new_len = (max(src, dst) + 1) as usize;
@@ -38,49 +33,34 @@ impl Graph {
     pub fn bfs(&self, src: u32) {
         let mut queue: VecDeque<u32> = VecDeque::new();
         let len = self.nodes.len() as usize;
-        // It calls .clone() on the first argument (n - 1) times, with the last value being moved into place. So, it works for any type implementing Clone, but doesn't create unnecessary copies.
-        let mut visited = vec![false; len];
-        
-        // only works with rust nightly:
-        // visited.resize(self.nodes.len(), false);
-        
+        let mut visited = vec!(false ; len);
         queue.push_front(src);
-        
         while let Some(current) = queue.pop_back() {
             if !visited[current as usize] {
                 println!("current: {}" , current);
                 visited[current as usize] = true;
-            }
+            } else { continue ; }
 
             for n in &self.nodes[current as usize] {
                 let neighbor: u32 = n.vertex;
-                if !visited[neighbor as usize] { queue.push_front(neighbor); }
+                queue.push_front(neighbor);
             }
         }
     }
     pub fn dfs(&self, src: u32) {
-        let mut current: u32 = src;
         let mut stack: Vec<u32> = Vec::new();
-        let mut visited: Vec<bool> = Vec::new();
-        
-        // only works with rust nightly:
-        // visited.resize(self.nodes.len(), false);
-        
         let len = self.nodes.len() as usize;
-        visited.extend(repeat(false).take(len));
-        stack.push(current);
-        
-        while stack.len() > 0 {
-            current = stack[0 as usize];
-            stack.remove(0);
+        let mut visited = vec!(false ; len);
+        stack.push(src);
+        while let Some(current) = stack.pop() {
             if !visited[current as usize] {
                 println!("current: {}" , current);
                 visited[current as usize] = true;
-            }
+            } else { continue ; }
 
             for n in &self.nodes[current as usize] {
                 let neighbor: u32 = n.vertex;
-                if !visited[neighbor as usize] { stack.push(neighbor); }
+                stack.push(neighbor);
             }
         }
     }
@@ -98,6 +78,7 @@ fn main() {
     g1.add_edge(2, 3, 3.0);
     g1.add_edge(3, 1, 6.0);
     g1.add_edge(3, 2, 3.0);
-    g1.bfs(0);
-    g1.dfs(0);
+    g1.bfs(3);
+    println!("\n");
+    g1.dfs(3);
 }
