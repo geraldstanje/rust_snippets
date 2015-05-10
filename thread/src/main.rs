4dc69a3de9d745ef207bc7de1fe9e4c299dac642
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-05-10 15:17:10 +0200

    added comment

--- thread/src/main.rs
+++ thread/src/main.rs
@@ -16,6 +16,9 @@ fn main() {
     
     for handle in thread_vec {
         // .unwrap() will propagate a panic from the child thread to the whole program
+        // .unwrap() just panics on None/Err(e)
+        // And returns the value from Some(x)/Ok(x)
+
         handle.join().unwrap();
     }
 }
