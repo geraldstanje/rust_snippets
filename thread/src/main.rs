b36967b59401b9548c9be2e1a096064e4d93c610
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-05-10 15:10:28 +0200

    added thread example

---
+++ thread/src/main.rs
@@ -0,0 +1,20 @@
+use std::thread;
+
+static NTHREADS: i32 = 10;
+
+// This is the `main` thread
+fn main() {
+    let mut thread_vec: Vec<_> = Vec::new();
+    
+    for i in 0..NTHREADS {
+        // Spawn up another thread
+        let child = thread::spawn(move || {
+            println!("this is thread number {}", i)
+        });
+        thread_vec.push(child);
+    }
+    
+    for handle in thread_vec {
+        handle.join().unwrap();
+    }
+}
