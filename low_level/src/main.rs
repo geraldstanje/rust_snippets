49081dc0cc651c327ff04a045f5510e1d758cb5f
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-05-21 23:43:57 +0200

    updated code

--- low_level/src/main.rs
+++ low_level/src/main.rs
@@ -1,13 +1,10 @@
-// useful for debugging:
-// strace -fo logfile /path/to/exe 
-// strace -fe mmap /path/to/executable
-
 use std::ptr;
 use std::fs;
 use std::io::{Write, SeekFrom, Seek};
 use std::os::unix::prelude::AsRawFd;
 use mmap::{MemoryMap, MapOption};
 use std::thread;
+use std::env;
 
 // from crates.io
 extern crate mmap;
@@ -15,40 +12,41 @@ extern crate libc;
 
 fn toggle(data: *mut u8, led_pin: u32) {
     unsafe {
-        *(data.offset(0x30) as *mut u32) ^= 1 << led_pin;
+        *(data.offset(16) as *mut u32) ^= 1 << led_pin;
     }
 }
 
 fn main() {
-    let size: usize = 0x7FFFFF; // i want to access the memory from 0x40000000 to 0x407FFFFF
+    if let Some(arg1) = env::args().nth(1) {
+        let size: usize = 0x1000;
 
-    let mut f = fs::OpenOptions::new().read(true)
-                                      .write(true)
-                                      .open("/dev/mem")
-                                      .unwrap();
+        let mut f = fs::OpenOptions::new().read(true)
+                                          .write(true)
+                                          .open("/dev/uio0")
+                                          .unwrap();
 
-    let mmap_opts = &[
-        // Then make the mapping *public* so it is written back to the file
-        MapOption::MapNonStandardFlags(libc::MAP_SHARED),
-        MapOption::MapReadable,
-        MapOption::MapWritable,
-        MapOption::MapOffset(0x40000000),
-        MapOption::MapFd(f.as_raw_fd()),
-    ];
+        let mmap_opts = &[
+            // Then make the mapping *public* so it is written back to the file
+            MapOption::MapNonStandardFlags(libc::MAP_SHARED),
+            MapOption::MapReadable,
+            MapOption::MapWritable,
+            MapOption::MapFd(f.as_raw_fd()),
+        ];
 
-    let mmap = MemoryMap::new(size, mmap_opts).unwrap();
+        let mmap = MemoryMap::new(size, mmap_opts).unwrap();
 
-    let data = mmap.data();
+        let data = mmap.data();
 
-    if data.is_null() {
-        panic!("Could not access data from memory mapped file")
-    }
-    else {
-        println!("successful data access to memory mapped file");
-    }
+        if data.is_null() {
+            panic!("Could not access data from memory mapped file")
+        }
+        else {
+            println!("successful data access to memory mapped file");
+        }
 
-    while (true) {
-        toggle(data, 3);
-        thread::sleep_ms(200);
+        while (true) {
+            toggle(data, 0);
+            thread::sleep_ms(arg1.parse::<u32>().unwrap());
+        }
     }
 }
