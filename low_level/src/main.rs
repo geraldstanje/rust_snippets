18808ebd87bcd20e4f0e99514913709f6b3dd5dd
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-05-28 23:25:06 +0200

    updated code

--- low_level/src/main.rs
+++ low_level/src/main.rs
@@ -16,36 +16,49 @@ fn toggle(data: *mut u8, led_pin: u32) {
     }
 }
 
+struct fpga_awg {
+    mmap: MemoryMap,
+    data: *mut u8,
+}
+
+impl fpga_awg {
+  pub fn new() -> fpga_awg {  
+    let size: usize = 0x1000;
+
+    let mut f = fs::OpenOptions::new().read(true)
+                                      .write(true)
+                                      .open("/dev/uio0")
+                                      .unwrap();
+
+    let mmap_opts = &[
+        // Then make the mapping *public* so it is written back to the file
+        MapOption::MapNonStandardFlags(libc::MAP_SHARED),
+        MapOption::MapReadable,
+        MapOption::MapWritable,
+        MapOption::MapFd(f.as_raw_fd()),
+    ];
+
+    let mmap = MemoryMap::new(size, mmap_opts).unwrap();
+
+    let data = mmap.data();
+
+    if data.is_null() {
+              panic!("Could not access data from memory mapped file")
+          }
+          else {
+                println!("successful data access to memory mapped file");
+          }
+
+    fpga_awg {mmap: mmap, data: data}
+  }
+}
+
 fn main() {
     if let Some(arg1) = env::args().nth(1) {
-        let size: usize = 0x1000;
-
-        let mut f = fs::OpenOptions::new().read(true)
-                                          .write(true)
-                                          .open("/dev/uio0")
-                                          .unwrap();
-
-        let mmap_opts = &[
-            // Then make the mapping *public* so it is written back to the file
-            MapOption::MapNonStandardFlags(libc::MAP_SHARED),
-            MapOption::MapReadable,
-            MapOption::MapWritable,
-            MapOption::MapFd(f.as_raw_fd()),
-        ];
-
-        let mmap = MemoryMap::new(size, mmap_opts).unwrap();
-
-        let data = mmap.data();
-
-        if data.is_null() {
-            panic!("Could not access data from memory mapped file")
-        }
-        else {
-            println!("successful data access to memory mapped file");
-        }
+        let mut awg = fpga_awg::new();
 
         while (true) {
-            toggle(data, 0);
+            toggle(awg.data, 1);
             thread::sleep_ms(arg1.parse::<u32>().unwrap());
         }
     }
