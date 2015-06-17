f587a4eecb693b7fe0085565304a95418bb299ab
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-06-18 00:24:10 +0200

    updated build script

--- c_to_rust/build.sh
+++ c_to_rust/build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-rustc add.rs
-gcc -Wall -o main main.c -L $(pwd) -ladd -lSystem -lpthread -lc -lm
+all() { 
+  rustc add.rs
+  gcc -Wall -o main main.c -L $(pwd) -ladd -lSystem -lpthread -lc -lm
+}
+
+
+clean() {
+  rm libadd.a
+  rm main
+}
+
+case $1 in all|clean) "$1" ;; *) printf >&2 '%s: unknown command\n' "$1"; exit 1;; esac
