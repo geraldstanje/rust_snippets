213e6651a8e27685658c165d572bffe14a68f27a
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-06-22 23:47:26 +0200

    added stack data structure impl

---
+++ stack/main.rs
@@ -0,0 +1,49 @@
+#![feature(box_syntax)]
+
+struct Node<T> {
+    data: T,
+    next: Option<Box<Node<T>>>,
+}
+
+struct Stack<T> {
+    head: Option<Box<Node<T>>>,
+}
+
+impl<T> Stack<T> {
+    fn new() -> Stack<T> {
+        Stack { head: None }
+    }
+
+    fn push(&mut self, elem: T) {
+        self.head = Some(box Node { 
+            data: elem, 
+            next: self.head.take(), 
+        });
+    }
+    
+    fn pop(&mut self) -> Option<T> {
+        match self.head.take() {
+            None => None,
+            Some(mut head) => {
+                self.head = head.next.take();
+                Some(head.data)
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut stack = Stack::new();
+    
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    println!("{:?}", stack.pop());
+    stack.push(4);
+    println!("{:?}", stack.pop());
+    println!("{:?}", stack.pop());
+    stack.push(5);
+    println!("{:?}", stack.pop());
+    println!("{:?}", stack.pop());
+    println!("{:?}", stack.pop());
+}
