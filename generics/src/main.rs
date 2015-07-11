c09761feaef80519d0aa376796be215d4b842587
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-07-12 01:29:11 +0200

    added generic example

---
+++ generics/src/main.rs
@@ -0,0 +1,33 @@
+use num::traits::One;
+use std::ops::Add;
+
+extern crate num;
+
+struct TwoNums<T> {
+  a: T,
+  b: T
+}
+
+// You implement function for struct TwuNums which accept type T with following traits: Add and One
+impl<T> TwoNums<T> where T: Add<Output=T> + One {
+  fn new(a: T, b: T) -> TwoNums<T> {
+    TwoNums {a: a, b: b}
+  }
+}
+
+// inside your funcion which accepts type your type T Rust expects that you pass to add() the variable 
+// with the same Type
+impl<T> TwoNums<T> where T: Add<Output=T> + One {
+  fn succ(self) -> TwoNums<T> {
+    let a = self.a.add(One::one());
+    let b = self.b.add(One::one());
+    TwoNums::new(a, b)
+  }
+}
+
+fn main() {
+  let num: TwoNums<f64> = TwoNums::new(1.0, 2.0);
+  let snum: TwoNums<f64> = num.succ();
+  println!("{}", snum.a);
+  println!("{}", snum.b);
+}
