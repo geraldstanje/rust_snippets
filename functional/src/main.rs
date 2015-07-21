8c9d885a98d412dc5cb990db8dae304abd0a92c0
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-07-21 21:12:11 +0200

    added closure example

---
+++ functional/src/main.rs
@@ -0,0 +1,15 @@
+struct MyStruct {
+    a: u32,
+    b: u32,
+}
+    
+fn main() {
+    let mut vec : Vec<MyStruct> = Vec::new();
+    
+    vec.push(MyStruct{a: 1, b: 2});
+    vec.push(MyStruct{a: 2, b: 2});
+    
+    for e in vec.into_iter().filter(|x| x.a > 1) {
+        println!("{}", e.a);
+    }
+}
