c64a4411f474d4927c76d764ccb0f5717e1cc8e9
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-07-30 23:42:04 +0200

    added string example

---
+++ string2/src/main.rs
@@ -0,0 +1,22 @@
+use std::borrow::Cow;
+
+fn remove_spaces<'a>(input: &'a str) -> Cow<'a, str> {
+    if input.contains(' ') {
+        let mut buf = String::with_capacity(input.len());
+
+        for c in input.chars() {
+            if c != ' ' {
+                buf.push(c);
+            }
+        }
+
+        return Cow::Owned(buf);
+    }
+
+    return Cow::Borrowed(input);
+}
+
+fn main() {
+    let s = remove_spaces("I love Rustlang");
+    println!("{}", s);
+}
